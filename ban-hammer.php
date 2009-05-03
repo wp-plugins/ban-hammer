568844ba2b1d85c5242df479c48d5a0252333fc6
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-03 17:42:21 +0000

    Version 1 - first release
    git-svn-id: https://plugins.svn.wordpress.org/ban-hammer/trunk@115284 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ban-hammer.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: Ban Hammer
+Plugin URI: http://code.google.com/p/ipstenu/
+Description: This plugin prevent people from registering with any email you list.
+Version: 0.3
+Author: Mika Epstein
+Author URI: http://www.ipstenu.org/
+
+Copyright 2009 Mika Epstein (email: ipstenu@ipstenu.org)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+// Here's the basic plugin
+function banhammer($user_login, $user_email, $errors) {
+        // Pre-2.6 compatibility
+        if(!defined('WP_PLUGIN_URL'))
+        {
+                if(!defined('WP_CONTENT_URL'))
+                { define('WP_CONTENT_URL', get_option('siteurl') . '/wp-content'); }
+                define('WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins');
+        }
+        if(!defined('WP_PLUGIN_DIR'))
+        {
+                if(!defined('WP_CONTENT_DIR'))
+                { define('WP_CONTENT_DIR', ABSPATH . 'wp-content'); }
+                define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins');
+        }
+
+        // First we check the users against StopForumSpam but ONLY if they checked the box
+        if (get_option('banhammer_stopforumspam') != '0' )
+        {
+                //Initialize the Curl session
+                $ch = curl_init();
+                $StopForumSpam = "http://www.stopforumspam.com/api?email=$user_email";
+                //Set curl to return the data instead of printing it to the browser.
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                //Set the URL
+                curl_setopt($ch, CURLOPT_URL, $StopForumSpam);
+                //Execute the fetch
+                $check = curl_exec($ch);
+                //Close the connection
+                curl_close($ch);
+
+                $test = "yes";
+                if ( strpos( $check, $test) > 0 )
+                {
+                        $errors->add('invalid_email', __( get_option('banhammer_message') ));
+                        return;
+                }
+        }
+
+        // Get blacklist
+        $blacklist_string = get_option('blacklist_keys');
+        $blacklist_array = explode("\n", $blacklist_string);
+        $blacklist_size = sizeof($blacklist_array);
+
+        // Go through blacklist
+        for($i = 0; $i < $blacklist_size; $i++)
+        {
+                $blacklist_current = trim($blacklist_array[$i]);
+                if(stripos($user_email, $blacklist_current) !== false)
+                {
+                        $errors->add('invalid_email', __( get_option('banhammer_message') ));
+                        return;
+                }
+        }
+}
+
+// Create the options for the message and spam assassin and set some defaults.
+function banhammer_activate() {
+        update_option('banhammer_stopforumspam', '0');
+        update_option('banhammer_message', '<strong>ERROR</strong>: Your email has been banned from registration.');
+}
+
+// Delete the options if the plugin is being turned off (pet peeve)
+function banhammer_deactivate() {
+        delete_option('banhammer_stopforumspam');
+        delete_option('banhammer_message');
+}
+
+// Load the options page
+function banhammer_optionsmenu() {
+        if (function_exists('add_submenu_page')) {
+          add_submenu_page('tools.php', 'Ban Hammer', 'Ban Hammer', '8', 'ban-hammer/ban-hammer_options.php');
+        }
+}
+
+// Hooks
+add_action('admin_menu', 'banhammer_optionsmenu');
+add_action('register_post', 'banhammer', 10, 3);
+
+register_activation_hook( __FILE__, 'banhammer_activate' );
+register_deactivation_hook( __FILE__, 'banhammer_deactivate' );
+?>
---
+++ readme.txt
@@ -0,0 +1,80 @@
+=== Ban Hammer ===
+Contributors: ipstenu
+Donate link: https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=BarlPvkfzQ-nAEgcKy65Jj7qMmPuyzlWNf9Sj-q0FfxaoaeZYqn77uxXXM0&dispatch=5885d80a13c0db1f998ca054efbdf2c25fe4a05bcb33bff6aad6e850552f81af
+Tags: access, admin, authentication, email, ban, mail, mailinator, register, registration, user, users
+Requires at least: 2.5
+Tested up to: 2.7.1
+Stable tag: 1.0
+
+Prevent people from registering with any email that is listed under your comment moderation blacklist.
+
+== Description ==
+
+We've all had this problem.  A group of spammers from mail.ru are registering to your blog, but you want to keep registration open.  How do you kill the spammers without bothering your clientele?  While you could edit your `functions.php` and block the domain, once you get past a few bad eggs, you have to escalate.
+
+Ban Hammer does that for you, preventing unwanted users from registering.
+
+Instead of using its own database table, Ban Hammer pulls from your list of blacklisted emails from the Comment Blacklist feature, native to WordPress.  Since emails never equal IP addresses, it simply skips over and ignores them.  This means you only have ONE place to update and maintain your blacklist.  When a blacklisted user attempts to register, they get a customizable message that they cannot register.
+
+In addition, Ban Hammer has built in support for StopForumSpam.com which can be turned on or off as desired.
+
+= Version 1.0 =
+
+**03 May, 2009**
+
+* First released version.
+
+= Version 0.3 =
+**30 March, 2009**
+
+* The error message is customizable.
+* Added support for StopForumSpam.com
+* Added in checkbox to use StopForumSpam (default to NO).
+* Cleans up after itself on deactivation (deletes the banhammer_* values from the wp_options table becuase I HATE when plugins leave themselves).
+
+= Version 0.2 =
+**29 March, 2009**
+
+* Shifted to use the WordPress comment blacklist as source. This was pretty much an 80% re-write from NDE's basis, keeping only the basic check at registration code.
+
+= Version 0.1 =
+**28 March, 2009**
+
+* First release using No Disposable Email's .dat file as a source.
+
+= History =
+Ban Hammer is a very weird fork of Philippe Paquet's No Disposable Email plugin (http://www.joeswebtools.com/wordpress-plugins/no-disposable-email/). The original plugin was a straight forward .dat file that listed all the bad emails (generally ones like mailinator that are disposable) and while Ban Hammer doesn't do that, this would not have been possible without that which was done before.
+
+Also credits to WP-Deadbolt, for making me think about SQL and TTC for StopForumSpam integration.
+
+MASSIVE credit to Travis Hamera for the StopForumSpam/cURL fix!
+
+== Installation ==
+
+1. Unpack the zip file and extract the `/ban-hammer/` folder and the files.
+2. Using an FTP program, upload the full `/ban-hammer/` folder to your WordPress plugins directory (Example: `/wp-content/plugins/`).
+3. Go to **Plugins > Installed** and activate the plugin.
+4. Go to **Tools > Ban Hammer** to customize the error message (and banned emails, but it's the same list from your comment moderation so...).
+
+== Screenshots ==
+
+1. Default Error message
+2. Admin screen
+
+== Frequently Asked Questions ==
+
+= If I change the blacklist via Ban Hammer, will it change the Comment Blacklist? =
+
+Yes! They are the exact same list, they use the same fields and they update the same data.  The only reason I put it there was I felt having an all-in-one place to get the data would be better.
+
+= Does this list the rejected registers? =
+
+No.  Since WordPress doesn't list rejected comments (your blacklist goes to a blackhole), I didn't bother with trying to do that here. If enough people think it's a need, I may consider it.
+
+= It breaks when I turn on StopForumSpam checking. Why? =
+
+At a guess, you don't have [http://us.php.net/curl cURL] support. You may need to check with your webhost about that. If they do have cURL setup, share the error with me and I'll try to debug!
+
+= Will you add other spam lists? =
+
+Sure. Or at least I'll try.  I'm not a genius so I may need help with APIs and the right PHP calls.
